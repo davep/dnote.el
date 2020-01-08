d3f28be2f939eaf182fe9139f7d9a4fc7218ecdc
Dave Pearson <dave.pearson@synpromics.com>
2020-01-08 17:02:14 +0000

    :card_index: Note that Emacs 24.3 or better is needed
    This is required now that we're using defvar-local.

--- dnote.el
+++ dnote.el
@@ -5,7 +5,7 @@
 ;; Version: 0.01
 ;; Keywords: {{keywords}}
 ;; URL: https://github.com/davep/dnote.el
-;; Package-Requires: ((emacs "24"))
+;; Package-Requires: ((emacs "24.3"))
 
 ;; This program is free software: you can redistribute it and/or modify it
 ;; under the terms of the GNU General Public License as published by the
